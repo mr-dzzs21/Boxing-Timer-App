3f61adb5250d3d29c102002bdd3c77e61032ad6d
Diyar <Diyar4775@gmail.com>
2026-01-25 00:04:02 +0100

    Initial Commit

---
+++ Boxing timer/Boxing_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boxing_timerApp.swift
+//  Boxing timer
+//
+//  Created by Diyar on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Boxing_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
